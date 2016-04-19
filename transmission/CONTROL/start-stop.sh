7c67c4c97d50cf7043a77127af939482af6fc17e
Fathi Boudra <fathi.boudra@linaro.org>
2016-04-19 15:56:02 +0300

    Add basic settings for transmission daemon

--- transmission/CONTROL/start-stop.sh
+++ transmission/CONTROL/start-stop.sh
@@ -2,10 +2,22 @@
 
 . /etc/script/lib/command.sh
 
+. /lib/lsb/init-functions
+
 APKG_PKG_DIR=/usr/local/AppCentral/transmission
 
 PATH=${APKG_PKG_DIR}/bin/:${PATH}
 DAEMON=transmission-daemon
+
+TRANSMISSION_HOME=${APKG_PKG_DIR}/etc
+TRANSMISSION_WEB_HOME=${APKG_PKG_DIR}/www
+
+# Avoid the error messages;
+#  UDP Failed to set receive buffer
+#  UDP Failed to set sent buffer
+# Set bigger sent and receive buffer
+${AS_SYSCTL} -w net.core.rmem_max = 16777216
+${AS_SYSCTL} -w net.core.wmem_max = 4194304
 
 case "$1" in
     start)
